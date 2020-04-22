05708731f188c74c87d0ad46bbd25ab7a2acc23b
Stephen McBride <stevemcbride3@gmail.com>
2020-04-22 17:35:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+
+  ROSTER == {}
+
+  def initialize(name)
+    @school_name = name
+  end
+
+  def add_student(name, grade)
+    ROSTER["#{grade}"] = [] if ROSTER["#{grade}"] == nil
+    ROSTER["#{grade}"].push("#{name}")
+  end
+
+  def grade(grade_number)
+    ROSTER["#{grade_number}"]
+  end
+
+  def sort
+    ROSTER["#{grade_number}"].sort
+  end
+
+end
